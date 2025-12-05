3734c9e3ee2fc43227040a6c7ec1056435152608
jiaqi <cody23333@gmail.com>
2025-12-05 14:54:52 +0800

    slam: 修改slam参数

--- miracrv_bringup/config/cartographer_miracz7_noimu.lua
+++ miracrv_bringup/config/cartographer_miracz7_noimu.lua
@@ -20,7 +20,9 @@ options = {
   use_landmarks = false,                    -- 是否使用landmark *
   num_laser_scans = 1,                      -- 是否使用单线激光数据，可订阅多个
   num_multi_echo_laser_scans = 0,           -- 是否使用multi_echo_laser_scans数据
-  num_subdivisions_per_laser_scan = 1,      -- 1帧数据被分成几次处理,一般为1
+  -- 运动畸变矫正，非常依赖里程计和IMU
+  num_subdivisions_per_laser_scan = 1,      -- 指定将一帧点云切成 N 份，然后均匀估算每一份的时间戳，一般为1
+
   num_point_clouds = 0,                     -- 是否使用点云数据
 
   lookup_transform_timeout_sec = 0.2,       -- 查找tf时的超时时间
@@ -28,7 +30,7 @@ options = {
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
 
-  rangefinder_sampling_ratio = 1.,          -- 传感器数据的采样频率
+  rangefinder_sampling_ratio = 1.,          -- 点云数据输入频率限制
   odometry_sampling_ratio = 1.,             -- 设成0.1即来10帧用1帧
   fixed_frame_pose_sampling_ratio = 1.,     -- 某个传感器不准，可以降低其使用频率
   imu_sampling_ratio = 1.,                  -- 如若不准，一般直接弃用即可
@@ -37,26 +39,42 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
+-- 用于限制点云的有效距离
 TRAJECTORY_BUILDER_2D.min_range = 0.15
-TRAJECTORY_BUILDER_2D.max_range = 10.0
+TRAJECTORY_BUILDER_2D.max_range = 5.0
+
+-- 运动畸变矫正，非常依赖里程计和IMU，里程计估算线速度，IMU 估算角速度
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1         -- 将 N 份点云合并成 1 份
+
+-- 体素滤波的大小设置，用来降低点云密度
+TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
+
+-- 用多少帧点云来构造一副 Submap，取值越小，每一副 Submap 内的累计误差就会越小
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 30
+
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+
+TRAJECTORY_BUILDER_2D.use_imu_data = false        -- 打开 IMU 数据输入
+
+-- 前端匹配器
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true -- RTCSM 匹配器开关
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 1  -- 在预测位置的平移半径内进行搜索，单位是米
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(90)  -- 在预测位置的旋转角度范围内进行搜索，单位是弧度
+
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 --  TEST 
 -- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 2e2
 -- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
--- TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1 
--- TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
--- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
+
 -- MAP_BUILDER.num_background_threads = 4
 
 --  TEST
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+
+-- 后端相关参数，可以将下面三个参数设置成 0 来关闭后端
 -- POSE_GRAPH.global_sampling_ratio = 0.001
 -- POSE_GRAPH.constraint_builder.sampling_ratio = 0.001
-
 -- POSE_GRAPH.optimize_every_n_nodes = 50
 
 return options
